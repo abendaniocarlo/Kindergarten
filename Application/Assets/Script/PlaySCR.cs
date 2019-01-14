57a20a55436f0ac133a7ca1319ec3298a5741b07
dlvg22 <30533251+dlvg22@users.noreply.github.com>
2019-01-15 06:09:08 +0800

    First Color Activity

--- Application/Assets/Script/PlaySCR.cs
+++ Application/Assets/Script/PlaySCR.cs
@@ -9,4 +9,10 @@ public class PlaySCR : MonoBehaviour {
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
+    public void GoColor()
+    {
+        SceneManager.LoadScene("Colors Activities");
+
+
+    }
 }
