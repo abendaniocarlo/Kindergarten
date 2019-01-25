880fb39cbb20fc80bcd51e94f562c781bf1dabbb
dlvg22 <30533251+dlvg22@users.noreply.github.com>
2019-01-25 10:24:50 +0800

    Catch game

---
+++ Application/Assets/Script/CatchScore.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CatchScore : MonoBehaviour {
+
+	// Use this for initialization
+    private Text TextScore;
+    public GameObject[] CatchObj;
+    private int score = 0;
+    private string[] myObj = {"Circle","Triangle","Square","Rectangle","Oblong"};
+    string Shape;
+    bool temp = true;
+    int KeyLog = 0;
+	void Start () {
+        Shape = myObj[KeyLog];
+        CatchObj[KeyLog].SetActive(true);
+           //  StartCoroutine("myShape");
+	}
+    void Awake() {
+        TextScore = GameObject.Find("ScoreText").GetComponent<Text>();
+        TextScore.text = "0";
+    }
+    void OnTriggerEnter2D(Collider2D target)
+    {
+     /*   if (target.tag !=  Shape)
+        {
+            transform.position = new Vector2(0, 100);
+            target.gameObject.SetActive(false);
+            StartCoroutine(RestartGame());
+        }*/
+        if (target.tag == Shape)
+        {
+            target.gameObject.SetActive(false);
+            score++;
+            if (score%5 == 0)
+            {
+                KeyLog++;
+                CatchObj[KeyLog].SetActive(true);
+                CatchObj[KeyLog-1].SetActive(false);
+                Shape = myObj[KeyLog];
+                
+                
+            }
+            TextScore.text = score.ToString();
+        }
+        else
+        {
+            transform.position = new Vector2(0, 100);
+            target.gameObject.SetActive(false);
+            StartCoroutine(RestartGame());
+
+
+        }
+        Debug.Log(Shape);
+    }
+    IEnumerator RestartGame()
+    {
+        yield return new WaitForSecondsRealtime(2f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    void Update()
+    {
+   
+    //    print(Shape);
+      
+    }
+
+    /*IEnumerator myShape()
+    {
+       
+       while(true){
+            yield return new WaitForSeconds(5.0f);
+            if (KeyLog >= 4)
+            {
+                KeyLog = 0;
+            }
+
+         
+            Shape = myObj[Random.Range(0,4)];
+            Debug.Log(Shape);
+        }
+    }*/
+}
