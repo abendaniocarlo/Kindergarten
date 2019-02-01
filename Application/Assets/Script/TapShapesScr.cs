6f03ba0870faebde2e8890d755ab48255a64f77a
abendaniocarlo <carlocabendanio@gmail.com>
2019-02-01 15:16:43 +0800

    tap shapes

---
+++ Application/Assets/Script/TapShapesScr.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TapShapesScr : MonoBehaviour {
+
+    public GameObject circle;
+    public GameObject square;
+    public GameObject triangle;
+    public GameObject rectangle;
+    public GameObject oval;
+
+    public Button circlebtn;
+    public Button squarebtn;
+    public Button trianglebtn;
+    public Button rectanglebtn;
+    public Button ovalbtn;
+
+    // Use this for initialization
+    void Start () {
+
+        circlebtn.onClick.AddListener(() => { StartCoroutine(circleTap()); });
+
+        squarebtn.onClick.AddListener(() => { StartCoroutine(squareTap()); });
+
+        trianglebtn.onClick.AddListener(() => { StartCoroutine(triangleTap()); });
+
+        rectanglebtn.onClick.AddListener(() => { StartCoroutine(rectangleTap()); });
+
+        ovalbtn.onClick.AddListener(() => { StartCoroutine(ovalTap()); });
+
+    }
+
+
+    IEnumerator circleTap()
+    {
+        circle.SetActive(true);
+
+        yield return new WaitForSeconds(1f);
+        circle.SetActive(false);
+    }
+
+    IEnumerator squareTap()
+    {
+        square.SetActive(true);
+
+        yield return new WaitForSeconds(1f);
+        square.SetActive(false);
+    }
+
+    IEnumerator triangleTap()
+    {
+        triangle.SetActive(true);
+
+        yield return new WaitForSeconds(1.5f);
+        triangle.SetActive(false);
+    }
+
+    IEnumerator rectangleTap()
+    {
+        rectangle.SetActive(true);
+
+        yield return new WaitForSeconds(2f);
+        rectangle.SetActive(false);
+    }
+
+    IEnumerator ovalTap()
+    {
+        oval.SetActive(true);
+
+        yield return new WaitForSeconds(1f);
+        oval.SetActive(false);
+    }
+
+
+    // Update is called once per frame
+    //void Update () {
+
+    //}
+}
