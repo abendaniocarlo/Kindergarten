bcbdaa7bd9cf393d32b2c51fc8c392dc41e23bab
dlvg22 <30533251+dlvg22@users.noreply.github.com>
2019-02-08 09:30:54 +0800

    videos

---
+++ Application/Assets/Script/VideoController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour {
+
+	// Use this for initialization
+    public GameObject PlayButton;
+    public GameObject PauseButton;
+    private VideoPlayer player;
+    void Awake()
+    {
+       
+        player = GetComponent<VideoPlayer>();
+        player.Pause();
+       
+    }
+	void Start () {
+        PauseButton.SetActive(false);
+	}
+    public void EXP()
+    {
+        
+    }
+    public void PlayPause()
+    {
+      
+        if (player.isPlaying)
+        {
+            player.Pause();
+            Debug.Log("pause");
+            PauseButton.SetActive(false);
+            PlayButton.SetActive(true);
+        }
+        else
+        {
+            player.Play();
+            Debug.Log("Play");
+            PlayButton.SetActive(false);
+            PauseButton.SetActive(true);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
