f2908ecb6ba094bcb807b420c660284062d0ccc5
dlvg22 <30533251+dlvg22@users.noreply.github.com>
2019-02-17 06:30:07 +0800

    Tutorials size sets pattern

---
+++ Application/Assets/Script/PatternTutorial2.cs
@@ -0,0 +1,531 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PatternTutorial2 : MonoBehaviour {
+
+    [SerializeField]
+    private Transform DropPoint;
+    private Vector2 initialPosition;
+    private float deltaX, deltaY;
+    string result;
+    string shape;
+  //  public GameObject Star;
+    public GameObject Board;
+ //   public GameObject[] Circles;
+    public GameObject[] Shapes;
+    public GameObject[] SGuide;
+  //  public GameObject[] LGuide;
+  //  public GameObject[] DropEffect;
+    private Vector2 mousePosition;
+    public string TagName;
+    public static string answer;
+    int[] YPosition = { 26, -64 };
+    int[] XPosition = { -143, -71, 0, 74, 146 };
+    public static int keyLog;
+  //  public GameObject ScoreBoard;
+   // public GameObject PanelBoard;
+    public static int myScore;
+    public static bool locked;
+
+    string value;
+
+    void Start()
+    {
+
+        initialPosition = transform.position;
+        //  TagName = PatternOne.TagName;
+
+    }
+    IEnumerator RestartGame()
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        locked = false;
+
+    }
+    public void Done()
+    {
+        SceneManager.LoadScene("DragTry");
+    }
+    public void Retry()
+    {
+        StartCoroutine("RestartGame");
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+        TagName = PatternOne.TagName;
+        //   Debug.Log(PatternOne.TagName);
+
+        if (Input.touchCount > 0 && !locked)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    answer = GetComponent<Collider2D>().tag;
+
+
+                    if (answer == "DrawBlue")
+                    {
+                        SGuide[0].SetActive(true);
+                        //  LGuide[0].SetActive(true);
+
+                    }
+                    else if (answer == "DrawBrown")
+                    {
+                        SGuide[1].SetActive(true);
+
+                    }
+                    else if (answer == "DrawYellow")
+                    {
+                        SGuide[2].SetActive(true);
+
+                    }
+                    else if (answer == "Bomb")
+                    {
+                        SGuide[3].SetActive(true);
+
+                    }
+                    else if (answer == "DrawGreen")
+                    {
+                        SGuide[4].SetActive(true);
+
+                    }
+                    else if (answer == "DrawOrange")
+                    {
+                        SGuide[5].SetActive(true);
+
+                    }
+                    else if (answer == "Shapes")
+                    {
+                        SGuide[6].SetActive(true);
+
+                    }
+                    else if (answer == "Circle")
+                    {
+                        SGuide[7].SetActive(true);
+
+                    }
+                    else if (answer == "DrawRed")
+                    {
+                        SGuide[8].SetActive(true);
+
+                    }
+                    else if (answer == "DrawViolet")
+                    {
+                        SGuide[9].SetActive(true);
+
+                    }
+                    else if (answer == "Square")
+                    {
+                        SGuide[10].SetActive(true);
+
+                    }
+                    else if (answer == "Rectangle")
+                    {
+                        SGuide[11].SetActive(true);
+
+                    }
+                    if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos))
+                    {
+
+                        deltaX = touchPos.x - transform.position.x;
+
+                        deltaY = touchPos.y - transform.position.y;
+
+                    }
+                    break;
+
+                case TouchPhase.Moved:
+                    if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos))
+                    {
+
+                        transform.position = new Vector2(touchPos.x - deltaX, touchPos.y - deltaY);
+                    }
+                    break;
+                case TouchPhase.Ended:
+                    if (answer == "DrawBlue")
+                    {
+                        SGuide[0].SetActive(true);
+                        //  LGuide[0].SetActive(true);
+
+                    }
+                    else if (answer == "DrawBrown")
+                    {
+                        SGuide[1].SetActive(false);
+
+                    }
+                    else if (answer == "DrawYellow")
+                    {
+                        SGuide[2].SetActive(false);
+
+                    }
+                    else if (answer == "Bomb")
+                    {
+                        SGuide[3].SetActive(false);
+
+                    }
+                    else if (answer == "DrawGreen")
+                    {
+                        SGuide[4].SetActive(false);
+
+                    }
+                    else if (answer == "DrawOrange")
+                    {
+                        SGuide[5].SetActive(false);
+
+                    }
+                    else if (answer == "Shapes")
+                    {
+                        SGuide[6].SetActive(false);
+
+                    }
+                    else if (answer == "Circle")
+                    {
+                        SGuide[7].SetActive(false);
+
+                    }
+                    else if (answer == "DrawRed")
+                    {
+                        SGuide[8].SetActive(false);
+
+                    }
+                    else if (answer == "DrawViolet")
+                    {
+                        SGuide[9].SetActive(false);
+
+                    }
+                    else if (answer == "Square")
+                    {
+                        SGuide[10].SetActive(false);
+
+                    }
+                    else if (answer == "Rectangle")
+                    {
+                        SGuide[11].SetActive(false);
+
+                    }
+                    if (Mathf.Abs(transform.position.x - DropPoint.position.x) <= 0.5f &&
+
+                    Mathf.Abs(transform.position.y - DropPoint.position.y) <= 0.5f)
+                    {
+                        if (answer == "DrawBlue")
+                        {
+                            Shapes[0].SetActive(true);
+                            //  LGuide[0].SetActive(true);
+
+                        }
+                        else if (answer == "DrawBrown")
+                        {
+                            Shapes[1].SetActive(true);
+
+                        }
+                        else if (answer == "DrawYellow")
+                        {
+                            Shapes[2].SetActive(true);
+
+                        }
+                        else if (answer == "Bomb")
+                        {
+                            Shapes[3].SetActive(true);
+
+                        }
+                        else if (answer == "DrawGreen")
+                        {
+                            Shapes[4].SetActive(true);
+
+                        }
+                        else if (answer == "DrawOrange")
+                        {
+                            Shapes[5].SetActive(true);
+
+                        }
+                        else if (answer == "Shapes")
+                        {
+                            Shapes[6].SetActive(true);
+
+                        }
+                        else if (answer == "Circle")
+                        {
+                            Shapes[7].SetActive(true);
+                        }
+                        else if (answer == "DrawRed")
+                        {
+                            Shapes[8].SetActive(true);
+
+                        }
+                        else if (answer == "DrawViolet")
+                        {
+                            Shapes[9].SetActive(true);
+
+                        }
+                        else if (answer == "Square")
+                        {
+                            Shapes[10].SetActive(true);
+
+                        }
+                        else if (answer == "Rectangle")
+                        {
+                            Shapes[11].SetActive(true);
+
+                        }
+                        keyLog++;
+                        transform.position = new Vector2(DropPoint.position.x, DropPoint.position.y);
+                        locked = true;
+                        answer = GetComponent<Collider2D>().tag;
+
+                        locked = false;
+
+
+                    }
+
+                    else
+                    {
+                        transform.position = new Vector2(initialPosition.x, initialPosition.y);
+                    }
+                    break;
+
+
+            }
+
+
+
+        }
+    }
+    private void OnMouseDown()
+    {
+        answer = GetComponent<Collider2D>().tag;
+
+
+        if (answer == "DrawBlue")
+        {
+            SGuide[0].SetActive(true);
+          //  LGuide[0].SetActive(true);
+
+        }
+        else if (answer == "DrawBrown")
+        {
+            SGuide[1].SetActive(true);
+           
+        }
+        else if (answer == "DrawYellow")
+        {
+            SGuide[2].SetActive(true);
+
+        }
+        else if (answer == "Bomb")
+        {
+            SGuide[3].SetActive(true);
+
+        }
+        else if (answer == "DrawGreen")
+        {
+            SGuide[4].SetActive(true);
+
+        }
+        else if (answer == "DrawOrange")
+        {
+            SGuide[5].SetActive(true);
+
+        }
+        else if (answer == "Shapes")
+        {
+            SGuide[6].SetActive(true);
+
+        }
+        else if (answer == "Circle")
+        {
+            SGuide[7].SetActive(true);
+
+        }
+        else if (answer == "DrawRed")
+        {
+            SGuide[8].SetActive(true);
+
+        }
+        else if (answer == "DrawViolet")
+        {
+            SGuide[9].SetActive(true);
+
+        }
+        else if (answer == "Square")
+        {
+            SGuide[10].SetActive(true);
+
+        }
+        else if (answer == "Rectangle")
+        {
+            SGuide[11].SetActive(true);
+
+        }
+        if (!locked)
+        {
+
+            deltaX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x;
+            deltaY = Camera.main.ScreenToWorldPoint(Input.mousePosition).y - transform.position.y;
+        }
+    }
+
+    private void OnMouseDrag()
+    {
+        if (!locked)
+        {
+
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector2(mousePosition.x - deltaX, mousePosition.y - deltaY);
+
+        }
+
+
+    }
+
+    private void OnMouseUp()
+    {
+        if (answer == "DrawBlue")
+        {
+            SGuide[0].SetActive(true);
+            //  LGuide[0].SetActive(true);
+
+        }
+        else if (answer == "DrawBrown")
+        {
+            SGuide[1].SetActive(false);
+
+        }
+        else if (answer == "DrawYellow")
+        {
+            SGuide[2].SetActive(false);
+
+        }
+        else if (answer == "Bomb")
+        {
+            SGuide[3].SetActive(false);
+
+        }
+        else if (answer == "DrawGreen")
+        {
+            SGuide[4].SetActive(false);
+
+        }
+        else if (answer == "DrawOrange")
+        {
+            SGuide[5].SetActive(false);
+
+        }
+        else if (answer == "Shapes")
+        {
+            SGuide[6].SetActive(false);
+
+        }
+        else if (answer == "Circle")
+        {
+            SGuide[7].SetActive(false);
+
+        }
+        else if (answer == "DrawRed")
+        {
+            SGuide[8].SetActive(false);
+
+        }
+        else if (answer == "DrawViolet")
+        {
+            SGuide[9].SetActive(false);
+
+        }
+        else if (answer == "Square")
+        {
+            SGuide[10].SetActive(false);
+
+        }
+        else if (answer == "Rectangle")
+        {
+            SGuide[11].SetActive(false);
+
+        }
+        if (Mathf.Abs(transform.position.x - DropPoint.position.x) <= 0.5f &&
+         Mathf.Abs(transform.position.y - DropPoint.position.y) <= 0.5f)
+        {
+            if (answer == "DrawBlue")
+            {
+                Shapes[0].SetActive(true);
+                //  LGuide[0].SetActive(true);
+
+            }
+            else if (answer == "DrawBrown")
+            {
+                Shapes[1].SetActive(true);
+
+            }
+            else if (answer == "DrawYellow")
+            {
+                Shapes[2].SetActive(true);
+
+            }
+            else if (answer == "Bomb")
+            {
+                Shapes[3].SetActive(true);
+
+            }
+            else if (answer == "DrawGreen")
+            {
+                Shapes[4].SetActive(true);
+
+            }
+            else if (answer == "DrawOrange")
+            {
+                Shapes[5].SetActive(true);
+
+            }
+            else if (answer == "Shapes")
+            {
+                Shapes[6].SetActive(true);
+
+            }
+            else if (answer == "Circle")
+            {
+                Shapes[7].SetActive(true);
+            }
+            else if (answer == "DrawRed")
+            {
+                Shapes[8].SetActive(true);
+
+            }
+            else if (answer == "DrawViolet")
+            {
+                Shapes[9].SetActive(true);
+
+            }
+            else if (answer == "Square")
+            {
+                Shapes[10].SetActive(true);
+
+            }
+            else if (answer == "Rectangle")
+            {
+                Shapes[11].SetActive(true);
+                Board.SetActive(true);
+
+            }
+            keyLog++;
+            transform.position = new Vector2(DropPoint.position.x, DropPoint.position.y);
+            locked = true;
+            answer = GetComponent<Collider2D>().tag;
+
+            locked = false;
+
+
+
+        }
+        else
+        {
+            transform.position = new Vector2(initialPosition.x, initialPosition.y);
+
+        }
+
+
+    }
+
+}
--- Application/Assets/Script/SortController.cs
+++ Application/Assets/Script/SortController.cs
@@ -6,41 +6,119 @@ public class SortController : MonoBehaviour {
     [SerializeField]
    // private Transform DropPoint;
     public GameObject[] myObject;
+  //  public GameObject[] Camel;
+    Transform Parent;
+    public GameObject[] Objects;
+    public GameObject[] setObjects;
+    public GameObject[] Boxes;
+    
     public int[] setIndex = { 0, 1, 2, 3, 4 };
     public int KeyLog;
+    int myIndex;
+    int Camel;
+    int Elephant;
+    int Walrus;
+    int Lion;
+    int Count;
+    int donkey;
+    int current;
     private Vector2 initialPosition;
     private float deltaX, deltaY;
     public static bool locked;
     public static string answer;
     private Vector2 mousePosition;
-    public string TagName;
+    public static string TagName;
+    int number = 0;
+    int Animal;
+    int[] PositionY = { 134, 75, 9, -57, -125 };
+    public static Rigidbody2D Controller;
+    
  	// Use this for initialization
 	void Start () {
+
         setIndex = randomPos(setIndex);
         initialPosition = transform.position;
-          // Instantiate(myObject[setIndex[KeyLog]], new Vector3(0, -3.7f, 0), transform.rotation);
-        
-      ;
+
+      
 	}
+    //void OnTriggerEnter2D(Collider2D target)
+    //{
+    //    ////Debug.Log(Controller.tag);
+    //    ////Debug.Log(target.tag);
+    //    //if (target.tag == Controller.tag)
+    //    //{
+    //    //    Destroy(Controller.gameObject);
+    //    //    locked = true;
+    //    //}
+        
+
+      
+    //}
 	
 	// Update is called once per frame
 	void Update () {
-        TagName = ShapeMatch.myTag;
-
+        if (Input.GetButtonUp("Fire1"))
+        {
+            foreach (GameObject Temp in GameObject.FindGameObjectsWithTag("Bomb"))
+            {
+                Destroy(Temp);
+            }
+        }
+       
+        if (Count == 20)
+        {
+            Debug.Log("Desigh Here");
+        }
         if (Input.touchCount > 0 && !locked)
         {
+           
+            Controller = GetComponent<Rigidbody2D>();
+            TagName = GetComponent<Rigidbody2D>().tag;
             Touch touch = Input.GetTouch(0);
             Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
             switch (touch.phase)
             {
                 case TouchPhase.Began:
-                    if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos))
+                     answer = GetComponent<Collider2D>().tag;
+                     Controller = GetComponent<Rigidbody2D>();
+                    TagName = GetComponent<Rigidbody2D>().tag;
+              
+                 //    Parent = GameObject.Find("Panel").transform;
+                    //if (Controller.tag == "Camel")
+                    //{
+
+                    //  GameObject InstantiatedGameObject =  Instantiate(Boxes[0], new Vector3(-6.5f, 0, 0), transform.rotation);
+                    //  InstantiatedGameObject.transform.SetParent(Parent);   
+          
+                    //}
+                    //else if (Controller.tag == "Donkey")
+                    //{
+
+                    //    GameObject InstantiatedGameObject = Instantiate(Boxes[1], new Vector3(-3, 0, 0), transform.rotation);
+                    //    InstantiatedGameObject.transform.SetParent(Parent);
+                    //}
+                    //else if (Controller.tag == "Elephant")
+                    //{
+                    //    GameObject InstantiatedGameObject = Instantiate(Boxes[2], new Vector3(1f, 0, 0), transform.rotation);
+                    //    InstantiatedGameObject.transform.SetParent(Parent);
+          
+                    //}
+                    //else if (Controller.tag == "Lion")
+                    //{
+
+                    //    GameObject InstantiatedGameObject = Instantiate(Boxes[3], new Vector3(3.7f, 0, 0), transform.rotation);
+                    //    InstantiatedGameObject.transform.SetParent(Parent);
+                    //}
+                    //else if (Controller.tag == "Walrus")
+                    //{
+
+                    //    GameObject InstantiatedGameObject = Instantiate(Boxes[4], new Vector3(6.5f, 0, 0), transform.rotation);
+                    //    InstantiatedGameObject.transform.SetParent(Parent);
+                    //}
+                    if (!locked)
                     {
-
-                        deltaX = touchPos.x - transform.position.x;
-
-                        deltaY = touchPos.y - transform.position.y;
-
+                       deltaX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x;
+                       deltaY = Camera.main.ScreenToWorldPoint(Input.mousePosition).y - transform.position.y;
                     }
                     break;
 
@@ -50,20 +128,22 @@ public class SortController : MonoBehaviour {
 
                         transform.position = new Vector2(touchPos.x - deltaX, touchPos.y - deltaY);
                     }
+
                     break;
                 case TouchPhase.Ended:
-                    //if (Mathf.Abs(transform.position.x - DropPoint.position.x) <= 0.5f &&
-
-                    //Mathf.Abs(transform.position.y - DropPoint.position.y) <= 0.5f)
-                    //{
-                    //    locked = true;
-                    //    transform.position = new Vector2(DropPoint.position.x, DropPoint.position.y);
-                    //}
-
-                    //else
-                    //{
-                    //    transform.position = new Vector2(initialPosition.x, initialPosition.y);
-                    //}
+                    foreach (GameObject Temp in GameObject.FindGameObjectsWithTag("Bomb"))
+                    {
+                        Destroy(Temp);
+                    }
+                    if (!locked)
+                    {
+                        transform.position = new Vector2(initialPosition.x, initialPosition.y);
+                    }
+                    else
+                    {
+                        locked = false;
+                        number++;
+                    }
                     break;
 
 
@@ -73,11 +153,98 @@ public class SortController : MonoBehaviour {
 
         }
 	}
+    void OnTriggerEnter2D(Collider2D target)
+    {
+     if(target.tag == Controller.tag)
+     {
+        // locked = true;
+             
+         if (Controller.tag == "Camel")
+         {
+             Animal = 0;
+             current =Camel;
+             Camel++;
+             
+          
+         }
+         if (Controller.tag == "Donkey")
+         {
+             Animal = 1;
+             current = donkey;
+             donkey++;
+             
+            
+         }
+         if (Controller.tag == "Elephant")
+         {
+             Animal = 2;
+             current = Elephant;
+             Elephant++;
+           
+         
+         }
+         if (Controller.tag == "Lion")
+         {
+             Animal = 3;
+            
+             current = Lion;
+             Lion++;
+            
+         }
+         if (Controller.tag == "Walrus")
+         {
+             Animal = 4;
+             current = Walrus;
+             Walrus++;
+        
+         
+         }
+         var createImage = Instantiate(setObjects[Animal], new Vector3(0, PositionY[current], 5), Quaternion.identity) as GameObject;
+         createImage.transform.SetParent(GameObject.Find(Controller.tag+"Drp").transform, false);
+         //Instantiate(myObject[0], new Vector3(0, -4.3f, 0), transform.rotation);
+     }
+
+
+    }
+ 
     private void OnMouseDown()
     {
 
         answer = GetComponent<Collider2D>().tag;
-        
+        Controller = GetComponent<Rigidbody2D>();
+        TagName = GetComponent<Rigidbody2D>().tag;
+      //  Parent = GameObject.Find("Panel").transform;
+        //if (Controller.tag == "Camel")
+        //{
+
+        //  GameObject InstantiatedGameObject =  Instantiate(Boxes[0], new Vector3(-6.5f, 0, 0), transform.rotation);
+        //  InstantiatedGameObject.transform.SetParent(Parent);   
+          
+        //}
+        //else if (Controller.tag == "Donkey")
+        //{
+
+        //    GameObject InstantiatedGameObject = Instantiate(Boxes[1], new Vector3(-3, 0, 0), transform.rotation);
+        //    InstantiatedGameObject.transform.SetParent(Parent);
+        //}
+        //else if (Controller.tag == "Elephant")
+        //{
+        //    GameObject InstantiatedGameObject = Instantiate(Boxes[2], new Vector3(1f, 0, 0), transform.rotation);
+        //    InstantiatedGameObject.transform.SetParent(Parent);
+          
+        //}
+        //else if (Controller.tag == "Lion")
+        //{
+
+        //    GameObject InstantiatedGameObject = Instantiate(Boxes[3], new Vector3(3.7f, 0, 0), transform.rotation);
+        //    InstantiatedGameObject.transform.SetParent(Parent);
+        //}
+        //else if (Controller.tag == "Walrus")
+        //{
+
+        //    GameObject InstantiatedGameObject = Instantiate(Boxes[4], new Vector3(6.5f, 0, 0), transform.rotation);
+        //    InstantiatedGameObject.transform.SetParent(Parent);
+        //}
         if (!locked)
         {
            deltaX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x;
@@ -87,38 +254,42 @@ public class SortController : MonoBehaviour {
 
     private void OnMouseDrag()
     {
+      
+     
+        
         if (!locked)
         {
             mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             transform.position = new Vector2(mousePosition.x - deltaX, mousePosition.y - deltaY);
         }
-        Debug.Log(mousePosition); 
+       
+        //Debug.Log(mousePosition); 
     }
 
-/*    private void OnMouseUp()
+   private void OnMouseUp()
     {
-        if (Input.mousePosition.x < GetComponent<Collider2D>().)
+       
+        if (!locked)
         {
-
-         
-            transform.position = new Vector2(mousePosition.x, Input.mousePosition.y);
-            locked = true;
+            transform.position = new Vector2(initialPosition.x, initialPosition.y);
         }
         else
         {
-            transform.position = new Vector2(initialPosition.x, initialPosition.y);
-
+            locked = false;
+            number++; 
         }
-    }    */
-    public int[] randomPos(int[] array)
-    {
-        for (int t = 0; t < array.Length; t++)
-        {
-            int tmp = array[t];
-            int r = Random.Range(t, array.Length);
-            array[t] = array[r];
-            array[r] = tmp;
-        }
-        return array;
+      
+     
     }
+   public int[] randomPos(int[] array)
+   {
+       for (int t = 0; t < array.Length; t++)
+       {
+           int tmp = array[t];
+           int r = Random.Range(t, array.Length);
+           array[t] = array[r];
+           array[r] = tmp;
+       }
+       return array;
+   }
 }
